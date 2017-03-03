d9eb50c0e90d07724601d32e8e0c025bfd5e83e9
lavan <lavanaggarwal7792@gmail.com>
2017-03-03 18:14:31 +0530

    Adding postgres in db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ var Sequelize = require('Sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-if(env === 'development'){
+if(env === 'production'){
 	sequelize = new Sequelize(process.env.DATABASE_URL , {
 
-		'dialect':'postgres'
+		dialect:'postgres'
 	});
 }else{
 	 sequelize = new Sequelize(undefined , undefined , undefined , {
